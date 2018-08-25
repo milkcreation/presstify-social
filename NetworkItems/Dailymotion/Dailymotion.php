7a3989b575b5dbfe9dc235e7c5bafe96e0b97dd4
Jordy Manner <jordy.manner@milkcreation.fr>
2018-08-25 18:19:10 +0000

    social -- tag 2.0.0 -- réécriture tify v2 serviceProvider + config

---
+++ NetworkItems/Dailymotion/Dailymotion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tiFy\Plugins\Social\NetworkItems\Dailymotion;
+
+use tiFy\Plugins\Social\NetworkItems\AbstractNetworkItem;
+
+class Dailymotion extends AbstractNetworkItem
+{
+
+}
