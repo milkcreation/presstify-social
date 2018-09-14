515d5dd6ca73796cb11ac594e857a86e910afe41
Jordy Manner <jordy.manner@milkcreation.fr>
2018-09-14 16:58:17 +0000

    social -- tag 1.4.7 -- Options adaptation compatibilité 1.4 + 2.0

---
+++ SocialServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace tiFy\Plugins\Social;
+
+use tiFy\App\Container\AppServiceProvider;
+use tiFy\Plugins\Social\Social;
+use tiFy\Plugins\Social\Admin\Options;
+use tiFy\Plugins\Social\Contracts\NetworkItemInterface;
+
+class SocialServiceProvider extends AppServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $singletons = [
+        Social::class,
+        Options::class
+    ];
+
+    /**
+     * Liste des réseaux déclarés.
+     * @var array
+     */
+    protected $networkItems = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $social = $this->app->resolve(Social::class);
+        $this->app->resolve(Options::class);
+
+        $networkItems = config()->has('social.networks')
+            ? config('social.networks', [])
+            : [
+                'dailymotion' => [],
+                'facebook'    => [],
+                'google-plus' => [],
+                'instagram'   => [],
+                'linkedin'    => [],
+                'pinterest'   => [],
+                'twitter'     => [],
+                'viadeo'      => [],
+                'vimeo'       => [],
+                'youtube'     => [],
+            ];
+
+        foreach ($networkItems as $name => $attrs) :
+            if (!$abstract = $social->getAbstract($name)) :
+                continue;
+            endif;
+
+            $concrete = $this->app
+                ->singleton($abstract)
+                ->build([$name, $attrs, $this->app]);
+
+            $this->networkItems[$name] = $concrete;
+        endforeach;
+    }
+
+    /**
+     * Récupération de la liste des réseaux déclarés.
+     *
+     * @return NetworkItemInterface[]
+     */
+    public function getNetworkItems()
+    {
+        return $this->networkItems;
+    }
+}
