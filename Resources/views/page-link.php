3e20518dab20f95bfa3f9fa05e9521260d47d558
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-29 13:11:49 +0000

    social -- tag 2.0.4 -- ajustement metabox

---
+++ Resources/views/page-link.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Social - Lien vers la page du compte d'un réseau.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\Social\Contracts\NetworkItemViewInterface $this
+ */
+?>
+
+<?php echo partial('tag', $this->all()); ?>
