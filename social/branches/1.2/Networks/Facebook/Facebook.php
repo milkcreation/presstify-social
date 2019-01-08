03c2544b7c606d4127f883c09a8af452ab17e7b4
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-01-08 12:23:54 +0000

    social -- branch 1.2 -- Déploiement initital

--- social/branches/1.2/Networks/Facebook/Facebook.php
+++ social/branches/1.2/Networks/Facebook/Facebook.php
@@ -1,68 +1,57 @@
 <?php
-
 namespace tiFy\Plugins\Social\Networks\Facebook;
-
-use tiFy\Plugins\Social\Social;
-use tiFy\Core\Options\Options;
 
 class Facebook extends \tiFy\Plugins\Social\Factory
 {
     /**
-     * CONSTRUCTEUR
-     *
-     * @return void
+     * Clé d'index de qualification des options
      */
-    public function __construct($attrs = [])
-    {
-        parent::__construct('fb', $attrs);
-
-        // Déclaration des événements
-        $this->appAddAction('init');
-        $this->appAddAction('wp_enqueue_scripts');
-        $this->appAddAction('wp_ajax_tify_fb_post2feed_callback', 'wp_ajax');
-        $this->appAddAction('wp_ajax_nopriv_tify_fb_post2feed_callback', 'wp_ajax');
-        $this->appAddAction('tify_options_register_node');
-
-        // Déclaration des fonctions d'aide à la saisie
-        $this->appAddHelper('tify_social_facebook_share', 'shareButton');
-        $this->appAddHelper('tify_social_facebook_page_link', 'pageLink');
-    }
+    protected static $OptionID              = 'fb';
+    
+    /**
+     * Liste des actions à déclencher
+     * @var string[]
+     * @see https://codex.wordpress.org/Plugin_API/Action_Reference
+     */
+    protected $tFyAppActions                = array(
+        'init',
+        'wp_enqueue_scripts',
+        'wp_ajax_tify_fb_post2feed_callback',
+        'wp_ajax_nopriv_tify_fb_post2feed_callback',
+        'tify_options_register_node'
+    );
 
     /**
-     * EVENEMENTS
+     * Cartographie des méthodes de rappel des actions
+     * @var mixed
+     */
+    protected $tFyAppActionsMethods         = array(
+        'wp_ajax_tify_fb_post2feed_callback'            => 'wp_ajax',
+        'wp_ajax_nopriv_tify_fb_post2feed_callback'     => 'wp_ajax',
+    );
+    
+    /**
+     * DECLENCHEURS
      */
     /**
      * Initialisation globale
-     *
-     * @return void
      */
     final public function init()
-    {
+    {        
+        require_once self::tFyAppDirname() ."/Helpers.php";
+    
         // Initialisation des scripts
-        \wp_register_style(
-            'tiFyPlugins-socialNetworksFacebook-share',
-            self::tFyAppUrl() . '/Share.css',
-            [],
-            160511
-        );
-        \wp_register_script(
-            'tiFyPlugins-socialNetworksFacebook-share',
-            self::tFyAppUrl() . '/Share.js',
-            ['jquery'],
-            160511,
-            true
-        );
+        wp_register_style('tiFyPluginsSocialNetworksFacebookShare', self::tFyAppUrl() . '/Share.css', array(), '160511');
+        wp_register_script('tiFyPluginsSocialNetworksFacebookShare', self::tFyAppUrl() . '/Share.js', array('jquery'), '160511', true);
     }
-
+    
     /**
      * Mise en file des scripts de l'interface utilisateur
-     *
-     * @return void
      */
     final public function wp_enqueue_scripts()
     {
-        \wp_enqueue_style('tiFyPlugins-socialNetworksFacebook-share');
-        \wp_enqueue_script('tiFyPlugins-socialNetworksFacebook-share');
+        wp_enqueue_style( 'tiFyPluginsSocialNetworksFacebookShare' );
+        wp_enqueue_script( 'tiFyPluginsSocialNetworksFacebookShare' );
     }
 
     /**
@@ -72,194 +61,121 @@ class Facebook extends \tiFy\Plugins\Social\Factory
     final public function wp_ajax()
     {
         $output = apply_filters('tify_fb_post2feed_callback_handle', '', $_POST['response'], $_POST['attrs']);
-
+        
         wp_die($output);
     }
-
+    
     /**
      * Déclaration de sections de boîte à onglets d'administration des options
-     *
-     * @return void
      */
     final public function tify_options_register_node()
     {
-        Options::registerNode(
-            [
-                'id'     => 'tiFyPlugins-socialNetworks-facebook',
-                'parent' => 'tiFyPlugins-socialNetworks',
-                'title'  => "<i class=\"fa fa-facebook-official\"></i> " . __('Facebook', 'tify'),
-                'cb'     => [$this, 'tabooxOptionsForm'],
-                'args'   => ['share' => $this->getAttr('share', false)]
-            ]
+        $attrs = self::getAttr();
+        tify_options_register_node(
+            array(
+                'parent'    => 'tiFyPluginSocial',
+                'id'        => 'tify_social-facebook',
+                'title'     => "<i class=\"fa fa-facebook-official\"></i> ". __( 'Facebook', 'tify' ),
+                'cb'        => "\\tiFy\\Plugins\\Social\\Networks\\Facebook\\Taboox\\Option\\PageLink\\Admin\\PageLink",
+                'args'      => array('share' => (isset($attrs['share']) ? $attrs['share'] : false))
+            )
         );
     }
-
-    /**
-     * Formulaire de saisie des options de réseau social
-     *
-     * @return void
-     */
-    final public function tabooxOptionsForm()
-    {
-        /**
-         * @var \tiFy\Plugins\Social\Networks\Facebook\Facebook $network
-         */
-        $network = self::tFyAppGetContainer('tiFy\Plugins\Social\Networks\Facebook\Facebook');
-
-        // Récupération des options
-        $defaults = [
-            'appId' => '',
-            'uri'   => ''
-        ];
-        $options = isset(Social::$Options['fb']) ? Social::$Options['fb'] : [];
-        $value = wp_parse_args($options, $defaults);
-
-        self::tFyAppGetTemplatePart('options', null, compact('network', 'value'));
-    }
-
+    
     /**
      * CONTROLEURS
      */
     /**
      * Lien de partage
-     * @see https://developers.facebook.com/docs/sharing/reference/feed-dialog/v2.6
-     *
-     * @param int $post_id Identifiant de qualification du post à partager
-     * @param array $args Attributs de configuration du partage
-     * @param bool $echo Activation de l'affichage
-     *
-     * @return null|string
+     * 
+     * @param int $post_id ID du post à partager
+     * @param array $args Attributs de partage
+     * @param bool $echo Affichage du lien
+     * 
+     * @return null|
      */
-    public static function shareButton($post = 0, $args = [], $echo = true)
+    public static function ShareButton($post = 0, $args = array(), $echo = true)
     {
-        static $Instance;
-        $Instance++;
-
-        /**
-         * @var \tiFy\Plugins\Social\Networks\Facebook\Facebook $network
-         */
-        $network = self::tFyAppGetContainer('tiFy\Plugins\Social\Networks\Facebook\Facebook');
+        static $Instance; $Instance++;
 
         // Initialisation de Facebook
-        $appId = $network->getOption('appId');
+        $appId = static::getOption('appId');
         $fb = new \tiFy\Lib\Facebook\Facebook($appId);
         $fb->JSInit();
-
-        if (!($post = get_post($post)) && is_singular()) :
+        
+        if (!($post = get_post($post)) && is_singular())
             $post = get_post();
-        endif;
-
-        $defaults = [
-            'id'     => '',
-            'class'  => '',
-            'href'   => '',
-            'text'   => false,
-            'icon'   => true,
-            'params' => [
-                'method' => 'share',
-            ],
-        ];
+        
+        $defaults = array(
+            'id'            => '',
+            'class'         => '',
+            'href'          => '',
+            'text'          => false,
+            'icon'          => true,
+            'params'        => array(
+                'method'        => 'share'
+            )
+        );
         $args = wp_parse_args($args, $defaults);
-        extract($args);
-
-        if ($icon && is_bool($icon)) :
+        extract( $args );
+        
+        if ($icon && is_bool($icon))
             $icon = \tiFy\Lib\Utils::get_svg(self::tFyAppDirname() . '/assets/logo.svg', false);
-        endif;
-
+        
         if ($params['method'] === 'feed') :
-            $defaults = [
+            /** @see https://developers.facebook.com/docs/sharing/reference/feed-dialog/v2.6 **/
+            $defaults = array(
                 #'app_id'           => $appId,
                 #'redirect_uri'     => '',
                 #'display'          => '',
                 #'from'             => (is_singular() && ! empty($post)) ? get_author_name($post->author_id) : '',
                 #'to'               => '',
-                'link'        => !empty($post) ? get_permalink($post->ID) : $href,
-                'picture'     => (!empty($post) && ($attachment_id = get_post_thumbnail_id($post->ID))) ? wp_get_attachment_url($attachment_id) : '',
+                'link'              => ! empty($post) ? get_permalink($post->ID) : $href,
+                'picture'           => (! empty($post) && ($attachment_id = get_post_thumbnail_id($post->ID))) ? wp_get_attachment_url($attachment_id) : '',
                 #'source'           => ''
-                'name'        => !empty($post) ? $post->post_title : get_bloginfo('name'),
-                'caption'     => '',
-                'description' => !empty($post) ? $post->post_excerpt : get_bloginfo('description'),
+                'name'              => ! empty($post) ? $post->post_title : get_bloginfo('name'),
+                'caption'           => '',
+                'description'       => ! empty($post) ? $post->post_excerpt : get_bloginfo('description'),
                 #'ref'              => '',
                 #'callback_attrs'   => array()
-            ];
+            );
             $params = is_array($params) ? wp_parse_args($params, $defaults) : $defaults;
-
+            
             if ($params['picture']) :
                 foreach (\tiFy\Lib\Facebook\Facebook::$OGImageSizes as $size) :
                     // Bypass
-                    if (!$picture = \tiFy\Lib\Utils::get_context_img_src($params['picture'], $size[0], $size[1],
-                        true)) {
+                    if (! $picture = \tiFy\Lib\Utils::get_context_img_src($params['picture'], $size[0], $size[1], true))
                         continue;
-                    }
-
+                    
                     $params['picture'] = $picture;
                     break;
                 endforeach;
             endif;
-
-            if (!$href && $params['link']) :
+            
+            if (! $href && $params['link']) :
                 $href = $params['link'];
             endif;
         else :
-            /** @see https://developers.facebook.com/docs/sharing/reference/share-dialog * */
-            if (!$href) :
+            /** @see https://developers.facebook.com/docs/sharing/reference/share-dialog **/
+            if(! $href) :
                 global $wp;
-                $href = home_url(add_query_arg([], $wp->request));
+                $href = home_url(add_query_arg(array(), $wp->request));
             endif;
-
-            $params['href'] = !empty($post) ? get_permalink($post->ID) : $href;
+            
+             $params['href'] = ! empty($post) ? get_permalink($post->ID) : $href;
         endif;
-
-        $output = "<a";
+        
+        $output  = "<a";
         $output .= " href=\"{$href}\"";
         $output .= " class=\"{$class}\"";
         $output .= " data-tiFyPluginSocialNetwork=\"facebook\"";
         $output .= " data-action=\"tify-fb-api_share_button\"";
-        $output .= " data-params=\"" . (htmlentities(json_encode($params))) . "\"";
+        $output .= " data-params=\"". ( htmlentities( json_encode( $params ) ) ) ."\"";
         $output .= ">{$icon}{$text}</a>";
-
-        if ($echo) :
+    
+        if ($echo)
             echo $output;
-        else :
-            return $output;
-        endif;
-    }
 
-    /**
-     * Lien vers la page/compte Facebook
-     *
-     * @param array $args Attributs de configuration du lien de la page
-     *
-     * @return string|void
-     */
-    public static function pageLink($args = [])
-    {
-        if (empty(Social::$Options['fb']['uri']))
-            return;
-
-        $defaults = [
-            'class' => '',
-            'text'  => __('Page Facebook', 'tify'),
-            'attrs' => [],
-            'echo'  => true
-        ];
-        $args = wp_parse_args($args, $defaults);
-        extract($args);
-
-        $output = "<a href=\"" . (Social::$Options['fb']['uri']) . "\" class=\"$class\"";
-        if (!isset($attrs['title']))
-            $output .= " title=\"" . sprintf(__('Vers la page Facebook du site %s', 'tify'), get_bloginfo('name')) . "\"";
-        if (!isset($attrs['target']))
-            $output .= " target=\"_blank\"";
-
-        foreach ((array)$attrs as $key => $value)
-            $output .= " {$key}=\"{$value}\"";
-        $output .= ">{$text}</a>";
-
-        if ($echo) :
-            echo $output;
-        else :
-            return $output;
-        endif;
+        return $output;
     }
 }
--- social/branches/1.2/Networks/GooglePlus/GooglePlus.php
+++ social/branches/1.2/Networks/GooglePlus/GooglePlus.php
@@ -1,215 +1,106 @@
 <?php
-
 namespace tiFy\Plugins\Social\Networks\GooglePlus;
 
-use tiFy\Plugins\Social\Social;
-use tiFy\Core\Options\Options;
+use tiFy\Plugins\Social\Factory;
 
-class GooglePlus extends \tiFy\Plugins\Social\Factory
+class GooglePlus extends Factory
 {
-    /**
-     * CONSTRUCTEUR
-     *
-     * @return void
-     */
-    public function __construct($attrs = [])
-    {
-        parent::__construct('googleplus', $attrs);
+	/* = ARGUMENTS = */
+	//
+	protected static $OptionID 				= 'googleplus';
+	
+	// Liste des Actions à déclencher
+	protected $tFyAppActions				= array(
+		'init',
+		'wp_enqueue_scripts',
+		'wp_footer',
+		'tify_options_register_node'
+	);
+	// Ordres de priorité d'exécution des actions
+	protected $tFyAppActionsPriority	= array(
+		'wp_footer' => 99
+	);
+		
+	/* = DECLENCHEURS = */
+	/** == Initialisation globale == **/
+	final public function init()
+	{		
+		require_once self::tFyAppDirname() ."/Helpers.php";
+		
+		// Initialisation des scripts
+		wp_register_style( 'tiFyPluginsSocialNetworksGooglePlusShare', self::tFyAppUrl() .'/Share.css', array(), '160511' );		
+		wp_register_script( 'tiFyPluginsSocialNetworksGooglePlusShare', self::tFyAppUrl() .'/Share.js', array( 'jquery', 'tiFyPluginsSocialNetworksTwitterWidgets'  ), '160629', true );	
+	}
+	
+	/** == Mise en file des scripts == **/
+	final public function wp_enqueue_scripts()
+	{
+		wp_enqueue_style( 'tiFyPluginsSocialNetworksGooglePlusShare' );
+		wp_enqueue_script( 'tiFyPluginsSocialNetworksGooglePlusShare' );
+	}
+	
+	/** == Pied de page du site == **/
+	final public function wp_footer()
+	{
+		?><script type="text/javascript">/* <![CDATA[ */
+		      window.___gcfg = {
+		        lang: '<?php echo get_locale();?>'
+		      };		
+		      (function() {
+		        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		        po.src = 'https://apis.google.com/js/plusone.js';
+		        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		      })();
+	      /* ]]> */</script><?php		
+	}
 
-        // Déclaration des événements
-        $this->appAddAction('init');
-        $this->appAddAction('wp_enqueue_scripts');
-        $this->appAddAction('wp_footer', null, 99);
-        $this->appAddAction('tify_options_register_node');
-
-        // Déclaration des fonctions d'aide à la saisie
-        $this->appAddHelper('tify_social_googleplus_share', 'shareButton');
-        $this->appAddHelper('tify_social_googleplus_page_link', 'pageLink');
-    }
-
-    /**
-     * EVENEMENTS
-     */
-    /**
-     * Initialisation globale
-     *
-     * @return void
-     */
-    final public function init()
-    {
-        // Initialisation des scripts
-        \wp_register_style(
-            'tiFyPlugins-socialNetworksGooglePlus-share',
-            self::tFyAppUrl() . '/Share.css',
-            [],
-            160511
-        );
-        \wp_register_script(
-            'tiFyPlugins-socialNetworksGooglePlus-share',
-            self::tFyAppUrl() . '/Share.js',
-            ['jquery'],
-            '160629',
-            true
-        );
-    }
-
-    /**
-     * Mise en file des scripts de l'interface utilisateur
-     *
-     * @return void
-     */
-    final public function wp_enqueue_scripts()
-    {
-        \wp_enqueue_style('tiFyPlugins-socialNetworksGooglePlus-share');
-        \wp_enqueue_script('tiFyPlugins-socialNetworksGooglePlus-share');
-    }
-
-    /**
-     * Pied de page de l'interface utilisateurs
-     *
-     * @return string
-     */
-    final public function wp_footer()
-    {
-        ?>
-        <script type="text/javascript">/* <![CDATA[ */
-            window.___gcfg = {
-                lang: '<?php echo get_locale();?>'
-            };
-            (function () {
-                var po = document.createElement('script');
-                po.type = 'text/javascript';
-                po.async = true;
-                po.src = 'https://apis.google.com/js/plusone.js';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(po, s);
-            })();
-            /* ]]> */</script><?php
-    }
-
-    /**
-     * Déclaration de sections de boîte à onglets d'administration des options
-     *
-     * @return void
-     */
-    final public function tify_options_register_node()
-    {
-        Options::registerNode(
-            [
-                'id'     => 'tiFyPlugins-socialNetworks-google_plus',
-                'parent' => 'tiFyPlugins-socialNetworks',
-                'title'  => "<i class=\"fa fa-google-plus\"></i> " . __('Google Plus', 'tify'),
-                'cb'     => [$this, 'tabooxOptionsForm']
-            ]
-        );
-    }
-
-    /**
-     * Formulaire de saisie des options de réseau social
-     *
-     * @return void
-     */
-    final public function tabooxOptionsForm()
-    {
-        /**
-         * @var \tiFy\Plugins\Social\Networks\GooglePlus\GooglePlus $network
-         */
-        $network = self::tFyAppGetContainer('tiFy\Plugins\Social\Networks\GooglePlus\GooglePlus');
-
-        // Récupération des options
-        $defaults = ['uri' => ''];
-        $value = isset(Social::$Options['gplus']) ? wp_parse_args(Social::$Options['gplus'], $defaults) : $defaults;
-
-        self::tFyAppGetTemplatePart('options', null, compact('network', 'value'));
-    }
-
-    /**
-     * CONTROLEURS
-     */
-    /**
-     * Lien de partage
-     *
-     * @param int $post_id Identifiant de qualification du post à partager
-     * @param array $args Attributs de configuration du partage
-     * @param bool $echo Activation de l'affichage
-     *
-     * @return null|string
-     */
-    public static function shareButton($post = 0, $args = [], $echo = true)
-    {
-        global $wp;
-
-        static $Instance;
-        $Instance++;
-
-        if (!($post = get_post($post)) && is_singular()) :
-            $post = get_post();
-        endif;
-
-        $defaults = [
-            'id'    => '',
-            'class' => '',
-            'text'  => false,
-            'icon'  => true,
-            'url'   => !empty($post) ? wp_get_shortlink($post->ID) : home_url(add_query_arg([], $wp->request)),
-        ];
-        $args = wp_parse_args($args, $defaults);
-        extract($args);
-
-        if ($icon && is_bool($icon)) :
-            $icon = \tiFy\Lib\Utils::get_svg(self::tFyAppDirname() . '/assets/logo.svg', false);
-        endif;
-
-        $output = "<a href=\"https://plus.google.com/share?url=" . esc_attr($url) . "\"" .
-            " onclick=\"javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;\"" .
-            " class=\"$class\"" .
-            " data-tiFyPluginSocialNetwork=\"googleplus\">" .
-            "{$icon}{$text}" .
-            "</a>";
-
-        if ($echo) :
-            echo $output;
-        else :
-            return $output;
-        endif;
-    }
-
-    /**
-     * Lien vers la page/compte GooglePlus
-     *
-     * @param array $args Attributs de configuration du lien de la page
-     *
-     * @return string|void
-     */
-    public static function pageLink($args = [])
-    {
-        if (empty(Social::$Options['gplus']['uri']))
-            return;
-
-        $defaults = [
-            'class' => '',
-            'text'  => '',
-            'attrs' => [],
-            'echo'  => true
-        ];
-        $args = wp_parse_args($args, $defaults);
-        extract($args);
-
-        $output = "<a href=\"" . Social::$Options['gplus']['uri'] . "\" class=\"$class\"";
-        if (!isset($attrs['title']))
-            $output .= " title=\"" . sprintf(__('Vers la page Google+ du site %s', 'tify'), get_bloginfo('name')) . "\"";
-
-        if (!isset($attrs['target']))
-            $output .= " target=\"_blank\"";
-
-        foreach ((array)$attrs as $key => $value)
-            $output .= " {$key}=\"{$value}\"";
-
-        $output .= ">$text</a>";
-
-        if ($echo)
-            echo $output;
-        else
-            return $output;
-    }
+	/** == Déclaration d'un section de boîte à onglets == **/
+	final public function tify_options_register_node(){
+		tify_options_register_node(	
+			array(
+				'id' 		=> 'tify_social_share-gplus',
+				'parent' 	=> 'tiFyPluginSocial',
+				'title' 	=> "<i class=\"fa fa-google-plus\"></i> ". __( 'Google Plus', 'tify' ),
+				'cb' 		=> "tiFy\\Plugins\\Social\\Networks\\GooglePlus\\Taboox\\Option\\PageLink\\Admin\\PageLink"	
+			)
+		);
+	}
+	
+	/* = AFFICHAGE = */
+	/** == Lien de partage == **/
+	public static function ShareButton( $post = 0, $args = array(), $echo = true )
+	{
+		global $wp;
+		
+		static $Instance; $Instance++;
+		
+		if( ! ( $post = get_post( $post ) ) && is_singular() )
+			$post = get_post();	
+	
+		$defaults = array(
+			'id'				=> '',
+			'class'				=> '',
+			'text'				=> false,
+			'icon'				=> true,
+			'url'				=> ! empty( $post ) ? wp_get_shortlink( $post->ID ) : home_url( add_query_arg( array(), $wp->request ) ),
+		);
+		$args = wp_parse_args( $args, $defaults );
+		extract( $args );
+		
+		if( $icon && is_bool( $icon ) )
+			$icon = \tiFy\Lib\Utils::get_svg( self::tFyAppDirname() .'/assets/logo.svg', false );
+		
+		
+		$output = 	"<a href=\"https://plus.google.com/share?url=". esc_attr( $url )."\"".
+					" onclick=\"javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;\"".
+					" class=\"$class\"".
+					" data-tiFyPluginSocialNetwork=\"googleplus\">".
+						"{$icon}{$text}".
+					"</a>";
+	
+		if( $echo )
+			echo $output;
+		else
+			return $output;
+	}
 }
