c988383ec666fa44b70d53214d142aa93c300f41
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-03-25 07:57:21 +0000

    social -- tag 2.0.9 -- Adaptations mise à jour tiFy (injection de dépendances)

---
+++ social/tags/2.0.9/Contracts/NetworkItemViewInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tiFy\Plugins\Social\Contracts;
+
+use tiFy\Contracts\View\ViewController;
+
+/**
+ * Interface NetworkItemViewInterface
+ * @package tiFy\Plugins\Social\Contracts
+ *
+ * @method bool isActive()
+ */
+interface NetworkItemViewInterface extends ViewController
+{
+
+}
---
+++ social/tags/2.0.9/NetworkItems/Vimeo/Vimeo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tiFy\Plugins\Social\NetworkItems\Vimeo;
+
+use tiFy\Plugins\Social\NetworkItems\AbstractNetworkItem;
+
+class Vimeo extends AbstractNetworkItem
+{
+
+}
