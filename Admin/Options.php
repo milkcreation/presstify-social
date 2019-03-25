c2c553d5a08ea13ccc76832da83b18ec2785eb8b
Jordy Manner <jordy.manner@milkcreation.fr>
2019-03-25 07:57:21 +0000

    social -- tag 2.0.9 -- Adaptations mise à jour tiFy (injection de dépendances)

---
+++ Admin/Options.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace tiFy\Plugins\Social\Admin;
+
+use tiFy\App\Dependency\AbstractAppDependency;
+use tiFy\Contracts\Metabox\MetaboxManager;
+use tiFy\Plugins\Social\Admin\Metabox\Options\NetworkOptions\NetworkOptions;
+use tiFy\Plugins\Social\Social;
+
+class Options
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param Social $social Instance du controleur principal.
+     *
+     * @return void
+     */
+    public function __construct(Social $social)
+    {
+        /** @var MetaboxManager $metabox */
+        $metabox = app('metabox');
+        $has_item = false;
+
+        foreach ($social->getItems() as $item) :
+            if ($item->isAdmin()) :
+                $has_item = true;
+
+                $metabox->add(
+                    "Social-{$item->getName()}",
+                    'tify_options@options',
+                    [
+                        'parent'  => 'Social',
+                        'content' => NetworkOptions::class,
+                        'args'    => ['network' => $item]
+                    ]
+                );
+            endif;
+        endforeach;
+
+        if ($has_item) :
+            $metabox->add(
+                'Social',
+                'tify_options@options',
+                [
+                    'title' => __('Réseaux sociaux', 'tify'),
+                    'position' => 99
+                ]
+            );
+
+            register_setting('tify_options', 'tify_social_share');
+        endif;
+    }
+}
---
+++ NetworkItems/NetworkItemView.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace tiFy\Plugins\Social\NetworkItems;
+
+use tiFy\View\ViewController;
+use tiFy\Plugins\Social\Contracts\NetworkItemViewInterface;
+
+/**
+ * Class NetworkItemBaseTemplate
+ * @package tiFy\Plugins\Social\NetworkItems
+ *
+ * @method string getIcon()
+ * @method string getStatus()
+ * @method bool getTitle()
+ * @method bool isActive()
+ */
+class NetworkItemView extends ViewController implements NetworkItemViewInterface
+{
+    /**
+     * Liste des méthodes héritées.
+     * @var array
+     */
+    protected $mixins = [
+        'getIcon',
+        'getStatus',
+        'getTitle',
+        'isActive'
+    ];
+
+    /**
+     * Translation d'appel des méthodes de l'application associée.
+     *
+     * @param string $name Nom de la méthode à appeler.
+     * @param array $arguments Liste des variables passées en argument.
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        if (in_array($name, $this->mixins)) :
+            return call_user_func_array(
+                [$this->engine->get('item'), $name],
+                $arguments
+            );
+        endif;
+    }
+}
