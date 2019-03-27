1b27353e19039628d49d88a4abe4155dcce0d666
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-03-27 15:42:44 +0000

    social -- branch 2.0 -- Maj tiFy Injection de dépendances

---
+++ social/tags/2.0.10/Social.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace tiFy\Plugins\Social;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use tiFy\Kernel\Tools;
+use tiFy\Plugins\Social\Contracts\NetworkItemInterface;
+
+/**
+ * Class Social
+ *
+ * @desc Extension PresstiFy de gestion des réseaux sociaux.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package tiFy\Plugins\Social
+ * @version 2.0.10
+ *
+ * USAGE :
+ * Activation
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans config/app.php ajouter \tiFy\Plugins\Social\SocialServiceProvider à la liste des fournisseurs de services.
+ * ex.
+ * <?php
+ * ...
+ * use tiFy\Plugins\Social\SocialServiceProvider;
+ * ...
+ *
+ * return [
+ *      ...
+ *      'providers' => [
+ *          ...
+ *          SocialServiceProvider::class
+ *          ...
+ *      ]
+ * ];
+ *
+ * Configuration
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans le dossier de config, créer le fichier social.php
+ * @see /vendor/presstify-plugins/social/Resources/config/social.php
+ */
+class Social
+{
+    use SocialResolverTrait;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        add_action('admin_enqueue_scripts', function () {
+                field('toggle-switch')->enqueue_scripts();
+
+                wp_register_style(
+                    'Social-adminOptions',
+                    class_info($this)->getUrl() . '/Resources/assets/css/admin-options.css',
+                    [],
+                    180822,
+                    'screen'
+                );
+
+                if (
+                    (get_current_screen()->id === 'settings_page_tify_options') &&
+                    config('social.admin_enqueue_scripts', true)
+                ) :
+                    wp_enqueue_style('Social-adminOptions');
+                endif;
+            });
+    }
+
+    /**
+     * Récupération du nom de la classe d'un controleur de réseau.
+     *
+     * @param string $alias
+     *
+     * @return string
+     */
+    public function getAbstract($alias)
+    {
+        $name = Str::studly($alias);
+        $abstract = "tiFy\\Plugins\\Social\\NetworkItems\\{$name}\\{$name}";
+
+        if (class_exists($abstract)) :
+            return $abstract;
+        endif;
+
+        return '';
+    }
+
+    /**
+     * Récupération de la liste des réseaux déclarés.
+     *
+     * @return NetworkItemInterface[]
+     */
+    public function getItems()
+    {
+        /** @var SocialServiceProvider $serviceProvider */
+        $serviceProvider = resolve(SocialServiceProvider::class);
+
+        return $serviceProvider->getNetworkItems();
+    }
+
+    /**
+     * Récupération de la liste des réseaux pris en charge affichable dans le menu par ordre d'affichage.
+     * {@internal le réseaux doit être actif, son url renseignée. La liste est triée par ordre d'affichage.}
+     *
+     * @return NetworkItemInterface[]
+     */
+    public function getMenuItems()
+    {
+        return (new Collection($this->getItems()))
+            ->filter(function ($item) {
+                /** @var NetworkItemInterface $item */
+                return ($item->isActive() === true) && ($item->hasUri() === true);
+            })
+            ->sortBy(function ($item) {
+                /** @var NetworkItemInterface $item */
+                return $item->getOrder();
+            })
+            ->all();
+    }
+
+    /**
+     * Récupération de l'icône d'un réseau.
+     *
+     * @param string $name Nom de qualification du réseau
+     *
+     * @return string
+     */
+    public function getNetworkIcon($name)
+    {
+        return Tools::File()->svgGetContents(__DIR__ . "/Resources/assets/networks/{$name}/img/icon.svg")
+            ? : '';
+    }
+
+    /**
+     * Affichage d'un menu de la liste des liens vers la page des comptes des réseaux.
+     *
+     * @param array $attrs Liste des attributs de configuration.
+     *
+     * @return string
+     */
+    public function menuRender($attrs = [])
+    {
+        Arr::set(
+            $attrs,
+            'attrs.class',
+            sprintf(
+                Arr::get($attrs, 'attrs.class', '%s'), 'Social-menu'
+            )
+        );
+
+        $attrs['items'] = $this->getMenuItems();
+
+        return $this->viewer('menu', $attrs);
+    }
+
+    /**
+     * Affichage d'un lien vers la page du compte d'un réseau.
+     *
+     * @param string $alias Nom de qualification du réseau.
+     * @param array $attrs Liste des attributs de configuration personnalisé.
+     *
+     * @return string
+     */
+    public function pageLinkRender($alias, $attrs = [])
+    {
+        /** @var NetworkItemInterface $networkItem */
+        $networkItem = container($this->getAbstract($alias));
+
+        return $networkItem->pageLink($attrs);
+    }
+}
---
+++ social/tags/2.0.10/SocialServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace tiFy\Plugins\Social;
+
+use tiFy\App\Container\AppServiceProvider;
+use tiFy\Plugins\Social\Admin\Options;
+use tiFy\Plugins\Social\Contracts\NetworkItemInterface;
+
+class SocialServiceProvider extends AppServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $singletons = [
+        Social::class,
+        Options::class
+    ];
+
+    /**
+     * Liste des réseaux déclarés.
+     * @var array
+     */
+    protected $networkItems = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $social = $this->app->resolve(Social::class);
+
+        $networkItems = config()->has('social.networks')
+            ? config('social.networks', [])
+            : [
+                'dailymotion' => [],
+                'facebook'    => [],
+                'google-plus' => [],
+                'instagram'   => [],
+                'linkedin'    => [],
+                'pinterest'   => [],
+                'twitter'     => [],
+                'viadeo'      => [],
+                'vimeo'       => [],
+                'youtube'     => [],
+            ];
+
+        foreach ($networkItems as $name => $attrs) :
+            if (!$abstract = $social->getAbstract($name)) :
+                continue;
+            endif;
+
+            $concrete = $this->app
+                ->singleton($abstract)
+                ->build([$name, $attrs, $social]);
+
+            $this->networkItems[$name] = $concrete;
+        endforeach;
+
+        $this->app->resolve(Options::class, [$social]);
+    }
+
+    /**
+     * Récupération de la liste des réseaux déclarés.
+     *
+     * @return NetworkItemInterface[]
+     */
+    public function getNetworkItems()
+    {
+        return $this->networkItems;
+    }
+}
