b0367c6dc60cdfafb2d22ae4d36eaf8d0bb4b0e2
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-08 11:47:30 +0000

    social -- tag 2.0.11 -- Réécriture injection de dépendance

---
+++ Resources/config/social.php
@@ -0,0 +1,26 @@
+<?php
+
+/** Exemple de configuration. */
+return [
+    /**
+     * Mise en file automatique des scripts de l'interface d'administration.
+     * @var boolean
+     */
+    'admin_enqueue_scripts' => true,
+
+    /**
+     * Attributs de configuration des réseaux sociaux.
+     * @var array
+     */
+    'networks'              => [
+        'dailymotion',
+        'facebook',
+        'google-plus',
+        'linkedin',
+        'pinterest',
+        'viadeo',
+        'vimeo',
+        'twitter',
+        'youtube'
+    ]
+];
