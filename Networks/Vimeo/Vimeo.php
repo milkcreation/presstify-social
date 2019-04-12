80f69a075d3a3a5e2fa903e360d8e7ca180b74c1
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-12 16:35:34 +0000

    social -- tag 2.0.12 -- Correctifs réécriture tiFy + injection de dépendances

--- Networks/Vimeo/Vimeo.php
+++ Networks/Vimeo/Vimeo.php
@@ -2,112 +2,21 @@
 
 namespace tiFy\Plugins\Social\Networks\Vimeo;
 
-use tiFy\Plugins\Social\Social;
-use tiFy\Core\Options\Options;
+use tiFy\Plugins\Social\Networks\NetworkFactory;
+use tiFy\Plugins\Social\Contracts\Social;
 
-class Vimeo extends \tiFy\Plugins\Social\Factory
+class Vimeo extends NetworkFactory
 {
     /**
-     * Clé d'index de qualification des options
-     * @var string
-     */
-    protected static $OptionID = 'vimeo';
-
-    /**
-     * CONSTRUCTEUR
+     * CONSTRUCTEUR.
+     *
+     * @param array $attrs Attributs de configuration.
+     * @param Social $social Instance du controleur principal.
      *
      * @return void
      */
-    public function __construct($attrs = [])
+    public function __construct($attrs, Social $social)
     {
-        parent::__construct($attrs);
-
-        // Déclaration des événements
-        $this->appAddAction('tify_options_register_node');
-
-        // Déclaration des fonctions d'aide à la saisie
-        $this->appAddHelper('tify_social_vimeo_page_link', 'pageLink');
-    }
-
-    /**
-     * EVENEMENTS
-     */
-    /**
-     * Déclaration de sections de boîte à onglets d'administration des options
-     *
-     * @return void
-     */
-    final public function tify_options_register_node()
-    {
-        Options::registerNode(
-            [
-                'id'     => 'tiFyPlugins-socialNetworks-vimeo',
-                'parent' => 'tiFyPlugins-socialNetworks',
-                'title'  => "<i class=\"fa fa-vimeo\"></i> " . __('Vimeo', 'tify'),
-                'cb'     => [$this, 'tabooxOptionsForm']
-            ]
-        );
-    }
-
-    /**
-     * Formulaire de saisie des options de réseau social
-     *
-     * @return void
-     */
-    final public function tabooxOptionsForm()
-    {
-        /**
-         * @var \tiFy\Plugins\Social\Networks\Vimeo\Vimeo $network
-         */
-        $network = self::tFyAppGetContainer('tiFy\Plugins\Social\Networks\Vimeo\Vimeo');
-
-        // Récupération des options
-        $defaults = ['uri' => ''];
-        $value = isset(Social::$Options['vimeo']) ? wp_parse_args(Social::$Options['vimeo'], $defaults) : $defaults;
-
-        self::tFyAppGetTemplatePart('options', null, compact('network', 'value'));
-    }
-
-    /**
-     * CONTROLEURS
-     */
-    /**
-     * Lien vers la chaine Vimeo
-     *
-     * @param array $args Attributs de configuration du lien de la page
-     *
-     * @return string|void
-     */
-    public static function pageLink($args = [])
-    {
-        if (empty(Social::$Options['vimeo']['uri']))
-            return;
-
-        $defaults = [
-            'class' => '',
-            'text'  => '',
-            'attrs' => [],
-            'echo'  => true
-        ];
-        $args = wp_parse_args($args, $defaults);
-        extract($args);
-
-        $output = "<a href=\"" . Social::$Options['vimeo']['uri'] . "\" class=\"$class\"";
-
-        if (!isset($attrs['title']))
-            $output .= " title=\"" . sprintf(__('Vers la chaîne Vimeo du site %s', 'tify'), get_bloginfo('name')) . "\"";
-
-        if (!isset($attrs['target']))
-            $output .= " target=\"_blank\"";
-
-        foreach ((array)$attrs as $key => $value)
-            $output .= " {$key}=\"{$value}\"";
-
-        $output .= ">{$text}</a>";
-
-        if ($echo)
-            echo $output;
-        else
-            return $output;
+        parent::__construct('vimeo', $attrs, $social);
     }
 }
