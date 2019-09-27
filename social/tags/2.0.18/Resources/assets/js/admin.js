6c39a15d319c4dc78a647d957c1a77a7716f8607
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-09-27 19:03:12 +0000

    ouch ...

--- social/tags/2.0.18/Resources/assets/js/admin.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-import 'presstify-framework/field/toggle-switch/js/scripts';
--- social/tags/2.0.18/Resources/views/network/page-link.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Social - Lien vers la page du compte d'un réseau.
- * ---------------------------------------------------------------------------------------------------------------------
- * @var tiFy\Plugins\Social\Contracts\NetworkViewer $this
- */
-?>
-<?php echo partial('tag', $this->all());
