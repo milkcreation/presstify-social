e5293306766e250860dc548f7c6d25659d7920f9
Jordy Manner <jordy.manner@milkcreation.fr>
2019-10-08 13:26:19 +0000

    social -- branch 2.0 -- Modification Réécriture métabox

--- Social.php
+++ Social.php
@@ -13,7 +13,7 @@ use tiFy\Plugins\Social\Contracts\{NetworkFactory, Social as SocialContract};
  * @desc Extension PresstiFy de gestion des réseaux sociaux.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy\Plugins\Social
- * @version 2.0.18
+ * @version 2.0.19
  *
  * USAGE :
  * Activation
