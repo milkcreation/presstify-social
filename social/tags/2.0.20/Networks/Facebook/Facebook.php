4428538c6f3b8957917804a1c41782abfcca2a89
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-10-25 07:22:30 +0000

    social -- tag 2.0.20 -- Passe relecture & réécriture

---
+++ social/tags/2.0.20/Networks/Facebook/Facebook.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tiFy\Plugins\Social\Networks\Facebook;
+
+use tiFy\Plugins\Social\Networks\NetworkFactory;
+use tiFy\Plugins\Social\Contracts\Social;
+
+class Facebook extends NetworkFactory
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param array $attrs Attributs de configuration.
+     * @param Social $social Instance du controleur principal.
+     *
+     * @return void
+     */
+    public function __construct($attrs, Social $social)
+    {
+        parent::__construct('facebook', $attrs, $social);
+    }
+}
---
+++ social/tags/2.0.20/Resources/assets/js/admin.js
@@ -0,0 +1,3 @@
+"use strict";
+
+import 'presstify-framework/field/toggle-switch/js/scripts';
