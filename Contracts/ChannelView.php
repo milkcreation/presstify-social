6f211656576b6026b16b11ecac7fd9ddce97090e
Jordy Manner <jordy.manner@milkcreation.fr>
2020-08-24 08:56:47 +0000

    social -- Compatibilité restatic 2.0

---
+++ Contracts/ChannelView.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Social\Contracts;
+
+use tiFy\Contracts\View\PlatesFactory;
+
+/**
+ * @method string getIcon()
+ * @method string getStatus()
+ * @method bool getTitle()
+ * @method bool isActive()
+ */
+interface ChannelView extends PlatesFactory
+{
+
+}
---
+++ Resources/views/app/page-link.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * @var tiFy\Plugins\Social\Contracts\ChannelView $this
+ */
+echo partial('tag', $this->all());
