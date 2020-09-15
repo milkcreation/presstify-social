c903f379cbd49165af14d33654cf59feaadfdc2c
Jordy Manner <jordy.manner@milkcreation.fr>
2020-09-15 08:16:11 +0000

    social v2.0.28 >> branch

---
+++ Resources/config/social.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /**
+     * Attributs de configuration des réseaux sociaux.
+     * @var array
+     */
+    'channel' => [
+        'dailymotion',
+        'facebook',
+        'google-plus',
+        'linkedin',
+        'pinterest',
+        'viadeo',
+        'vimeo',
+        'twitter',
+        'youtube',
+    ]
+];
