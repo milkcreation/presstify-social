9d46b253277705ba167714a3ad47e7a1d88a0b71
Jordy Manner <jordy.manner@milkcreation.fr>
2020-11-14 10:37:58 +0000

    social v2.0.x-dev

---
+++ src/Channel/TwitterChannel.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Social\Channel;
+
+use tiFy\Plugins\Social\Contracts\ChannelDriver as ChannelDriverContract;
+use tiFy\Support\Proxy\Url;
+
+class TwitterChannel extends ChannelDriver
+{
+    /**
+     * Url de partage et indicateur de partage possible sur le réseau.
+     * @see https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent
+     * @var string
+     */
+    protected $sharer = 'https://twitter.com/intent/tweet';
+
+    /**
+     * @param array $attrs Attributs de configuration.
+     *
+     * @return void
+     */
+    public function __construct(array $attrs = [])
+    {
+        parent::__construct('twitter', $attrs);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDeeplink(): string
+    {
+        $uri = Url::set($this->get('uri'))->get();
+        $id = ltrim(rtrim($uri->getPath(), '/'), '/');
+
+        if ($this->isAndroidOS()) {
+            return "intent://twitter.com/{$id}#Intent;package=com.twitter.android;scheme=https;end";
+        } elseif ($this->isIOS()) {
+            return "twitter://user?screen_name={$id}";
+        }
+
+        return '';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setPostShare($post): ChannelDriverContract
+    {
+        $this->share_params = [
+            'text' => str_replace('|', '', strip_tags($post->getTitle())),
+            'url'  => $post->getPermalink(),
+        ];
+
+        return $this;
+    }
+}
