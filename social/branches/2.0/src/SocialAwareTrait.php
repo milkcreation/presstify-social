9a5e8228fda3531dfb9da0e3dcebd7e3bceb01d7
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-11-14 10:37:58 +0000

    social v2.0.x-dev

---
+++ social/branches/2.0/src/SocialAwareTrait.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Social;
+
+use Exception;
+use tiFy\Plugins\Social\Contracts\Social;
+
+trait SocialAwareTrait
+{
+    /**
+     * Instance du gestionnaire de réseaux sociaux.
+     * @var Social|null
+     */
+    protected $social;
+
+    /**
+     * Récupération de l'instance du gestionnaire de réseaux sociaux.
+     *
+     * @return Social|null
+     */
+    public function social(): ?Social
+    {
+        if (is_null($this->social)) {
+            try {
+                $this->social = Social::instance();
+            } catch (Exception $e) {
+                $this->social;
+            }
+        }
+
+        return $this->social;
+    }
+
+    /**
+     * Définition du gestionnaire de réseaux sociaux.
+     *
+     * @param Social $social
+     *
+     * @return static
+     */
+    public function setSocial(Social $social): self
+    {
+        $this->social = $social;
+
+        return $this;
+    }
+}
